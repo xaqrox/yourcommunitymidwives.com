0dbaa89193f8bd98bf31ab5e090ff401f3dea120
Xaq Rothman <xaq.rox@gmail.com>
2014-03-23 19:03:03 -0400

    Handle midwives with no website.

--- midwife.tpl.php
+++ midwife.tpl.php
@@ -6,6 +6,8 @@
 		<p class="fax"><span class="glyphicon glyphicon-print"></span>&nbsp;<?php print($midwife['fax']); ?></p>
 	<?php endif; ?>
 	<p class="email"><span class="glyphicon glyphicon-envelope"></span>&nbsp;<a href="mailto:<?php print($midwife['email']); ?>"><?php print($midwife['email']); ?></a></p>
-	<p class="web"><span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a></p>
+	<?php if (isset($midwife['web'])): ?>
+		<p class="web"><span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a></p>
+	<?php endif; ?>
 	<p class="tag"><?php print($midwife['tag']); ?>
 </div>
