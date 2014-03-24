42a49dca6aaadaad2bd4734093981bb140ae585b
Xaq Rothman <xaq.rox@gmail.com>
2014-03-23 21:50:38 -0400

    Indent obsessively.

--- midwife.tpl.php
+++ midwife.tpl.php
@@ -11,17 +11,27 @@
 		</p>
 	</div>
 
-	<p class="phone"><span class="glyphicon glyphicon-earphone"></span>&nbsp;<?php print($midwife['phone']); ?></p>
+	<p class="phone">
+		<span class="glyphicon glyphicon-earphone"></span>&nbsp;<?php print($midwife['phone']); ?>
+	</p>
 
 	<?php if (isset($midwife['fax'])): ?>
-		<p class="fax"><span class="glyphicon glyphicon-print"></span>&nbsp;<?php print($midwife['fax']); ?></p>
+		<p class="fax">
+			<span class="glyphicon glyphicon-print"></span>&nbsp;<?php print($midwife['fax']); ?>
+		</p>
 	<?php endif; ?>
 
-	<p class="email"><span class="glyphicon glyphicon-envelope"></span>&nbsp;<a href="mailto:<?php print($midwife['email']); ?>"><?php print($midwife['email']); ?></a></p>
+	<p class="email">
+		<span class="glyphicon glyphicon-envelope"></span>&nbsp;<a href="mailto:<?php print($midwife['email']); ?>"><?php print($midwife['email']); ?></a>
+	</p>
 
 	<?php if (isset($midwife['web'])): ?>
-		<p class="web"><span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a></p>
+		<p class="web">
+			<span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a>
+		</p>
 	<?php endif; ?>
 
-	<p class="tag"><span class="glyphicon glyphicon-comment"></span>&nbsp;<?php print($midwife['tag']); ?>
+	<p class="tag">
+		<span class="glyphicon glyphicon-comment"></span>&nbsp;<?php print($midwife['tag']); ?>
+	</p>
 </div>
