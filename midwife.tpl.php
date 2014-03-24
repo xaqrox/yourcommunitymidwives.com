0fbff9b67fcb9805c4189f9d21b5c058433ecc02
Xaq Rothman <xaq.rox@gmail.com>
2014-03-23 21:55:29 -0400

    Wrap everything in divs.

--- midwife.tpl.php
+++ midwife.tpl.php
@@ -11,27 +11,37 @@
 		</p>
 	</div>
 
-	<p class="phone">
-		<span class="glyphicon glyphicon-earphone"></span>&nbsp;<?php print($midwife['phone']); ?>
-	</p>
+	<div class="phone">
+		<p>
+			<span class="glyphicon glyphicon-earphone"></span>&nbsp;<?php print($midwife['phone']); ?>
+		</p>
+	</div>
 
 	<?php if (isset($midwife['fax'])): ?>
-		<p class="fax">
-			<span class="glyphicon glyphicon-print"></span>&nbsp;<?php print($midwife['fax']); ?>
-		</p>
+		<div class="fax">
+			<p>
+				<span class="glyphicon glyphicon-print"></span>&nbsp;<?php print($midwife['fax']); ?>
+			</p>
+		</div>
 	<?php endif; ?>
 
-	<p class="email">
-		<span class="glyphicon glyphicon-envelope"></span>&nbsp;<a href="mailto:<?php print($midwife['email']); ?>"><?php print($midwife['email']); ?></a>
-	</p>
+	<div class="email">
+		<p>
+			<span class="glyphicon glyphicon-envelope"></span>&nbsp;<a href="mailto:<?php print($midwife['email']); ?>"><?php print($midwife['email']); ?></a>
+		</p>
+	</div>
 
 	<?php if (isset($midwife['web'])): ?>
-		<p class="web">
-			<span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a>
-		</p>
+		<div class="web">
+			<p>
+				<span class="glyphicon glyphicon-globe"></span>&nbsp;<a href="http://<?php print($midwife['web']); ?>"><?php print($midwife['web']); ?></a>
+			</p>
+		</div>
 	<?php endif; ?>
 
-	<p class="tag">
-		<span class="glyphicon glyphicon-comment"></span>&nbsp;<?php print($midwife['tag']); ?>
-	</p>
+	<div class="tag">
+		<p>
+			<span class="glyphicon glyphicon-comment"></span>&nbsp;<?php print($midwife['tag']); ?>
+		</p>
+	</div>
 </div>
