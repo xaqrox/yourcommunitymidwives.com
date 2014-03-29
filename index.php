3016a620401095fd8a92c281f0a3e979c8a48d80
Xaq Rothman <xaq.rox@gmail.com>
2014-03-28 21:52:18 -0400

    OMG GOOGLE FONTS

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Tangerine|Courgette' rel='stylesheet' type='text/css'>
     </head>
     <body>
         <!--[if lt IE 7]>
