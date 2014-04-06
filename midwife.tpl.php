9064e9f60be8d96802af19658a9614bc301813f2
Xaq Rothman <xaq.rox@gmail.com>
2014-04-06 15:34:28 -0400

    Header height voodoo.

--- midwife.tpl.php
+++ midwife.tpl.php
@@ -1,31 +1,33 @@
 <div class="col-md-3 midwife" id="<?php print($midwife['id']) ?>">
-	<div class="header row text-center <?php if (isset($midwife['titleinlogo'])) print('no-title'); ?>">
-		<?php if (isset($midwife['img'])): ?>
-			<div class="logo">
-				<?php if (isset($midwife['web'])): ?>
-					<a href="http://<?php print($midwife['web']); ?>">
-				<?php endif; ?>
+	<div class="header-wrapper row text-center">
+		<div class="header <?php if (isset($midwife['titleinlogo'])) print('no-title'); ?>">
+			<?php if (isset($midwife['img'])): ?>
+				<div class="logo">
+					<?php if (isset($midwife['web'])): ?>
+						<a href="http://<?php print($midwife['web']); ?>">
+					<?php endif; ?>
 
-				<img title="<?php print(strip_tags($midwife['name'])); ?>" class="img-responsive" src="midwives/logo/<?php print($midwife['img']); ?>" />
+					<img title="<?php print(strip_tags($midwife['name'])); ?>" class="img-responsive" src="midwives/logo/<?php print($midwife['img']); ?>" />
 
-				<?php if (isset($midwife['web'])): ?>
-					</a>
-				<?php endif; ?>
-			</div>
-		<?php endif; ?>
+					<?php if (isset($midwife['web'])): ?>
+						</a>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
 
-			<h2 class="<?php if (isset($midwife['titleinlogo'])) print('sr-only'); ?>">
-				<?php if (isset($midwife['web'])): ?>
-					<a href="http://<?php print($midwife['web']); ?>">
-				<?php endif; ?>
+				<h2 class="<?php if (isset($midwife['titleinlogo'])) print('sr-only'); ?>">
+					<?php if (isset($midwife['web'])): ?>
+						<a href="http://<?php print($midwife['web']); ?>">
+					<?php endif; ?>
 
-				<?php print($midwife['name']); ?>
+					<?php print($midwife['name']); ?>
 
-				<?php if (isset($midwife['web'])): ?>
-					</a>
-				<?php endif; ?>
-			</h2>
+					<?php if (isset($midwife['web'])): ?>
+						</a>
+					<?php endif; ?>
+				</h2>
 
+		</div>
 	</div>
 
 	<div class="address clearfix">
